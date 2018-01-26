f99d10ebd38055a66e39776283c465a6bdaf9a1e
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-26 21:56:06 +0900

    home 기본 셋팅

---
+++ front/src/pages/HomePage/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import API_HOST from '../../config';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        home
+      </div>
+    );
+  }
+}
+
+export default Home;
